81c4c643059a594d51737dc9ee5eb728a15913c0
Ethan Borkosky <ethan.borkosky@gmail.com>
2020-04-17 16:53:37 -0400

    update bootstrap styling

--- src/Home.js
+++ src/Home.js
@@ -3,10 +3,15 @@ import React from 'react';
 export default class Home extends React.Component {
     render() {
         return (
-            <div>
-                Welcome!
-                <br/>
-                Home Screen
+            <div className="container text-center">
+                <div className="row">
+                    <div className="col-12">
+
+                        <div className="display-4 font-weight-bold">Welcome</div>
+                        <br />
+                Here's the latest
+                    </div>
+                </div>
             </div>
         );
     }
