509f00653013438fb297be625b22dd187f9980ec
Ethan Borkosky <ethan.borkosky@gmail.com>
2020-04-24 22:33:42 -0400

    replace 'profile' nav item with username

--- src/Nav.js
+++ src/Nav.js
@@ -1,10 +1,8 @@
 import React from 'react';
 
 export default class NavBar extends React.Component {
-
-
-
     render() {
+        let userpage = "/user/" + localStorage.getItem('username');
         return (
             <nav className="navbar navbar-expand-lg navbar-dark bg-danger">
                 <a className="navbar-brand" href="/">Sauce'd</a>
@@ -16,9 +14,16 @@ export default class NavBar extends React.Component {
                         <a className="nav-item nav-link" href="/">Home <span className="sr-only">(current)</span></a>
                         <a className="nav-item nav-link" href="/feed">Feed</a>
                         <a className="nav-item nav-link" href="/communities">Communities</a>
-                        <a className="nav-item nav-link" href="/profile">Profile</a>
+                        {/* <a className="nav-item nav-link" href="/profile">Profile</a> */}
+                    </div>
+                    <div className="nav navbar-nav ml-auto">
+                        <h5 className="mt-2 mr-4">
+                            <a className="nav-item text-white" href={userpage}>
+                                {this.props.username}
+                            </a>
+                        </h5>
                         {this.props.loginStatus === true ?
-                            <a className="nav-item nav-link" href="/logout">Logout</a>
+                            <a className="nav-item text-dark mt-2" href="/logout">Logout</a>
                             :
                             <a className="nav-item nav-link" href="/login">Login</a>
                         }
