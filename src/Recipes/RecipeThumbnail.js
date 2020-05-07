8d3029ab6b701f03f87007e93ef1649d9fb5dd45
Ethan Borkosky <ethan.borkosky@gmail.com>
2020-05-06 23:48:19 -0400

    add id for body text css class

--- src/Recipes/RecipeThumbnail.js
+++ src/Recipes/RecipeThumbnail.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import './RecipeThumbnail.css';
 
 export default class RecipeThumbnail extends React.Component {
     render() {
         return (
-            <div>
+            <div className>
                 <h4>{this.props.title}</h4>
                 <p>Difficulty: {this.props.difficulty}/5</p>
-                <p><strong>Ingredients:</strong> {this.props.ingredients}</p>
-                <p><strong>Steps</strong> <br />{this.props.body}</p>
+                <p id="body-text"><strong>Ingredients:</strong> {this.props.ingredients}</p>
+                <div id="body-text"><strong>Steps</strong> <br />{this.props.body}</div>
             </div>
         )
     }
