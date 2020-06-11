df0525575fa8b263ffbce23ed5aa8f99301fe691
Ethan Borkosky <ethan.borkosky@gmail.com>
2020-06-11 01:14:54 -0400

    ...

--- src/Recipes/RecipeThumbnail.js
+++ src/Recipes/RecipeThumbnail.js
@@ -7,7 +7,7 @@ export default class RecipeThumbnail extends React.Component {
             <div className>
                 <h4>{this.props.title}</h4>
                 <p>Difficulty: {this.props.difficulty}/5</p>
-                <p id="body-text"><strong>Ingredients:</strong> {this.props.ingredients}</p>
+                <p id="body-text"><strong>Ingredients:</strong><br /> {this.props.ingredients}</p>
                 <div id="body-text"><strong>Steps</strong> <br />{this.props.body}</div>
             </div>
         )
