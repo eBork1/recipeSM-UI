00975cc85124886e7698faee6e245bae2d53387e
Ethan Borkosky <ethan.borkosky@gmail.com>
2020-06-29 15:21:48 -0400

    add link

--- src/Recipes/RecipeThumbnail.js
+++ src/Recipes/RecipeThumbnail.js
@@ -3,12 +3,14 @@ import './RecipeThumbnail.css';
 
 export default class RecipeThumbnail extends React.Component {
     render() {
+        let recipeLink = "http://localhost:3000/recipe/" + this.props.id;
         return (
             <div className>
                 <h4>{this.props.title}</h4>
                 <p>Difficulty: {this.props.difficulty}/5</p>
                 <p id="body-text"><strong>Ingredients:</strong><br /> {this.props.ingredients}</p>
                 <div id="body-text"><strong>Steps</strong> <br />{this.props.body}</div>
+                <a href={recipeLink} className="btn btn-danger">Full Recipe</a>
             </div>
         )
     }
